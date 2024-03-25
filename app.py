e1062d10d93050278cb422a4b496534b1154399c
José Rocha <48589830+joserocha94@users.noreply.github.com>
2024-03-25 22:40:15 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
     return 'Hello, Docker!'
 
+
 @app.route('/goodbye')
 def goodbye_world():
     return 'Goodbye, Docker!'    
+
 
 @app.route('/testing/')
 def test_world():
